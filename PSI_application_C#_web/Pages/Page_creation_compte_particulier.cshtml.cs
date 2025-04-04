cd0724416fbea921bd1b7b73ea0df94f9afaa453
Théotime ARCHAS <theotime.archas@edu.devinci.fr>
2025-04-04 23:53:59 +0200

    correction adresse valide

--- PSI_application_C#_web/Pages/Page_creation_compte_particulier.cshtml.cs
+++ PSI_application_C#_web/Pages/Page_creation_compte_particulier.cshtml.cs
@@ -146,7 +146,7 @@ namespace PSI_application_C__web.Pages
             bool adresse_code_postal_valide = saisie_adresse_code_postal != null && saisie_adresse_code_postal.Length > 0;
             bool num_tel_valide = EstNumeroTelCorrect(saisie_num_tel);
             bool adresse_valide = await Adresse_a_coordonees.GetCoords(saisie_adresse_num_rue + " " + saisie_adresse_nom_rue,
-                saisie_adresse_ville, saisie_adresse_code_postal, "France") == false;
+                saisie_adresse_ville, saisie_adresse_code_postal, "France");
             bool adresse_mail_valide = EstAdresseMailCorrect(saisie_adresse_mail);
             if (id_utilisateur_valide == false)
             {
@@ -183,6 +183,7 @@ namespace PSI_application_C__web.Pages
             if (adresse_valide == false)
             {
                 ViewData["Erreur_adresse_reseau"] = "Notre servie ne dessert pas cette adresse.";
+                Console.WriteLine("Pourquoi l'erreur ne s'affiche pas");
             }
             if (num_tel_valide == false)
             {
