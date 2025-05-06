537ae4e159f4e895e4fb09f629495ad227b44105
Hugo ANTAKI <hugo.antaki@edu.devinci.fr>
2025-05-06 11:15:55 +0200

    ajout des convertisseur en JSON et XML

--- PSI_application_C#_web/Pages/Page_mon_profile.cshtml.cs
+++ PSI_application_C#_web/Pages/Page_mon_profile.cshtml.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Xml.Linq;
 
 namespace PSI_application_C__web.Pages
 {
@@ -62,5 +65,54 @@ namespace PSI_application_C__web.Pages
                 Console.WriteLine(e.ToString());
             }
         }
+
+        public JsonObject StatUtilisateurToJson(List<string> StatUtilisateur)
+        {
+            var utilisateur = new JsonObject
+            {
+                ["ID_utilisateur"] = StatUtilisateur[0],
+                ["Mot_de_passe_utilisateur"] = StatUtilisateur[1],
+                ["info_personnel"] = new JsonObject
+                {
+                    ["Nom_utilisateur"] = StatUtilisateur[2],
+                    ["Prénom_utilisateur"] = StatUtilisateur[3],
+                    ["Adresse_utilisateur"] = StatUtilisateur[4],
+                    ["Num_tel_utilisateur"] = StatUtilisateur[5],
+                    ["adresse_mail_utilisateur"] = StatUtilisateur[6],
+                },
+                ["info_entreprise"] = new JsonObject
+                {
+                    ["Utilisateur_est_entreprise"] = StatUtilisateur[7],
+                    ["Nom_entreprise"] = StatUtilisateur[8],
+                },
+                ["Nbre_signalements_contre_utilisateur"] = StatUtilisateur[9]
+            };
+
+            return utilisateur;
+        }
+
+        public XElement StatUtilisateurToXml(List<string> StatUtilisateur)
+        {
+            var utilisateurXml = new XElement("Utilisateur",
+                new XElement("ID_utilisateur", StatUtilisateur[0]),
+                new XElement("Mot_de_passe_utilisateur", StatUtilisateur[1]),
+                new XElement("info_personnel",
+                    new XElement("Nom_utilisateur", StatUtilisateur[2]),
+                    new XElement("Prénom_utilisateur", StatUtilisateur[3]),
+                    new XElement("Adresse_utilisateur", StatUtilisateur[4]),
+                    new XElement("Num_tel_utilisateur", StatUtilisateur[5]),
+                    new XElement("adresse_mail_utilisateur", StatUtilisateur[6])
+                ),
+                new XElement("info_entreprise",
+                    new XElement("Utilisateur_est_entreprise", StatUtilisateur[7]),
+                    new XElement("Nom_entreprise", StatUtilisateur[8])
+                ),
+                new XElement("Nbre_signalements_contre_utilisateur", StatUtilisateur[9])
+            );
+
+            return utilisateurXml;
+        }
+
     }
 }
+
